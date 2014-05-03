28a7b1cfe3ecc39007c51aae94091c5d0cc19020
Dennis Zepeda <dennis.zepeda@hotmail.com>
2014-05-03 12:15:55 -0600

    Vista de Login

---
+++ application/views/login.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<title>Transamerica</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <script src="js/jquery-2.1.0.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>    
+</head>
+<body>
+	    <div class="col-lg-3 col-md-3 col-sm-2 hidden-xs"></div>
+    <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+    <div class="row jumbotron text-center" style="font-size:16px;">
+    <fieldset>
+        <legend class="text-center">Login</legend>
+
+        <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-3 hidden-xs"  style="text-align:right"><label>Usuario</label></div>
+        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12"  style="text-align:left"><input type="text" name="user" class="form-control" placeholder="Usuario"/>            
+        </div>
+        </div>
+        <div class="row" style="margin-top: 10px;">
+        <div class="col-lg-3 col-md-3 col-sm-3 hidden-xs"  style="text-align:right"><label>Contrase&ntilde;a</label></div>
+        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12"  style="text-align:left"><input type="password" class="form-control" name="password" placeholder="Password" />
+        </div>
+        </div>
+       
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" style="margin-top: 10px;">
+            
+            
+        <input type="submit"  value="Sign In" name="signin"/>
+            
+            <br />            
+            
+        </div>
+    </fieldset>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-2  hidden-xs"></div>
+</body>
+</html>
