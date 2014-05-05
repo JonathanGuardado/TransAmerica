583d1fc22c1548c7f4dab676025be89db17b64e4
Dennis Zepeda <dennis.zepeda@hotmail.com>
2014-05-05 00:37:27 -0600

    Cambios en CSS en los contents

--- application/views/Administrator/content_flotas.php
+++ application/views/Administrator/content_flotas.php
@@ -1,5 +1,5 @@
 <div class="row">
-	<div id="listgroup" class="col-lg-2" style="margin-top:60px">
+	<div id="listgroup" class="col-lg-2 col-md-2 col-sm-2 col-xs-12" style="margin-top:60px">
 		<div class="list-group accordion" id="accordion">
 			<div class="accordion-group">
 				<div class="accordion-heading">
@@ -61,7 +61,7 @@
 		  	
 		</div>
 	</div>
-	<div id="content_flotas" class="col-lg-10">
+	<div id="content_flotas" class="col-lg-10 col-md-10 col-sm-10 col-xs-12">
 		<h1>Flota</h1>
 	</div>
 </div>
