f74cd912e9e873bcea0760dc1193daf533ba93d0
Mark Rivas <riv_mark1991rok@hotmail.com>
2014-05-09 16:51:46 -0600

    Todas las funciones de Manteimiento en Funcionamiento

--- application/views/Administrator/Desechar/desecharWheel.php
+++ application/views/Administrator/Desechar/desecharWheel.php
@@ -8,7 +8,7 @@
         </div>
         <div class="row" style="margin-top: 10px;">
         <div class="col-lg-3 col-md-3 col-sm-3 hidden-xs"  style="text-align:right"><label>No. Llanta</label></div>
-        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12"  style="text-align:left"><input type="text" class="form-control" name="noWheel" placeholder="No. Llanta" />
+        <div class="col-lg-9 col-md-9 col-sm-9 col-xs-12"  style="text-align:left"><input type="text" data-controller="wheel" data-method="getDataWheels" class="form-control autocomplete" name="noWheel" placeholder="No. Llanta" />
         </div>
         </div>
         <div class="row" style="margin-top: 10px;">
