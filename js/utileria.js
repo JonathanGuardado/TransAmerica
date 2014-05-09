b1590910e510b5daec532f365867b7bdf068fb97
JonathanGuardado <jonathan.guardado11@gmail.com>
2014-05-08 22:37:09 -0600

    Se agrego la validacion a los campos vacios

--- js/utileria.js
+++ js/utileria.js
@@ -1,18 +1,24 @@
 jQuery(document).ready(function() {
-    jQuery(".sendBtn").click(envioDatos);
+    jQuery(".sendBtn").click(function() {
+        if (InputsEmpty(this) == 0) {
+            envioDatos();
+        }
+    });
     var input = jQuery(".autocomplete");
-    if(input.length>0){
-    loadAutocomplete(input);
+    if (input.length > 0) {
+        loadAutocomplete(input);
     }
 });
-function loadAutocomplete(input) {    
+
+
+function loadAutocomplete(input) {
     var form = jQuery(input).parents("form");
     var dataMetod = jQuery(input).attr("data-method");
     var dataController = jQuery(input).attr("data-controller");
     var url = jQuery(form).attr("action");
     //url = url.substring(0, url.lastIndexOf("/"));
     //url = url.substring(0, url.lastIndexOf("/")) ;
-    url= dataController+"/"+dataMetod;
+    url = dataController + "/" + dataMetod;
     jQuery.ajax({
         url: url,
         type: 'GET',
@@ -39,4 +45,28 @@ function envioDatos() {
             alert(descripcion);
         }
     })
+}
+function InputsEmpty(btnSend) {
+    countInputEmpty = 0;
+    jQuery("input[type='text']").each(function(i, input) {
+        if (jQuery(input).val().trim() == "") {
+            jQuery(input).css({"border": "1px solid red"});
+            div=jQuery(btnSend).parent();             
+            setTimeout(function() {
+                jQuery(input).css({"border": "1px solid #ccc"})
+            }, 3000);
+            countInputEmpty++;
+        }
+    });
+    if (countInputEmpty>0){
+        divCount=jQuery("#divError").length;
+        if(divCount==0){
+        jQuery("<div id='divError' style='color:red'>¡Ops Le faltaron algunos campos!</div>").appendTo(div)
+        }else{
+            jQuery("#divError").html("¡Ops Le faltaron algunos campos!");
+        }
+          setTimeout(function() {
+            jQuery("#divError").html(""); }, 3000);
+    }
+    return countInputEmpty;
 }
